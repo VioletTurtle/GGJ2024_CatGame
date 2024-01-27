a680e8b8b06cce45a56bdaad4bbd076f738215c0
amagee99 <107876195+amagee99@users.noreply.github.com>
2024-01-27 11:32:03 -0500

    Chimney Puzzle init

---
+++ Assets/Script/Bookshelf.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Bookshelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/Playermovement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{   private Vector3 playerMovement;
+    [SerializeField] float playerSpeed = 10f;
+    [SerializeField] float jumpForce = 20f;
+    [SerializeField] Rigidbody rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        playerMovement = new Vector3(-Input.GetAxis("Horizontal"), 0f, -Input.GetAxis("Vertical"));
+        MovePlayer();
+    }
+    void MovePlayer(){
+        Vector3 moveInput = transform.TransformDirection(playerMovement) * playerSpeed;
+        rb.velocity = new Vector3(moveInput.x, rb.velocity.y, moveInput.z);
+        if(Input.GetKeyDown(KeyCode.Space)){
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
